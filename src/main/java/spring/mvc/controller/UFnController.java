bdf29d3faed396e373251a226e22d6678a324ce4
박재승 <conscience98@naver.com>
2023-05-16 10:58:16 +0900

    로그인유무수정_0516

--- src/main/java/spring/mvc/controller/UFnController.java
+++ src/main/java/spring/mvc/controller/UFnController.java
@@ -52,6 +52,7 @@ public class UFnController {
    public ModelAndView home(HttpSession session, String h_num, String s_um) {
       ModelAndView model = new ModelAndView();
       String u_id = (String) session.getAttribute("loginId");
+      String loginStatus = (String) session.getAttribute("loginStatus");
       System.out.println(u_id);
       UserDto dto = service.findUserdataById(u_id);
       ResumeDto rdto = uservice.getResume(dto.getU_num());
@@ -60,7 +61,10 @@ public class UFnController {
       List<EnterpriseDto> getMypageLikeEnter=uservice.getMypageLikeEnter(dto.getU_num());//세션의 아이디 통해 dto를 갖고오고 그 dto통해 U_num갖고오기, U_num통해 관심 기업과 관심 공고 갖고옴
       List<Map<String, Object>> getMypageScrapPosting=uservice.getMypageScrapPosting(dto.getU_num());
      
-      
+      if(loginStatus==null) {
+	    	String loginmessage = "로그인 후 사용 가능합니다.";
+	    	model.addObject("loginmessage", loginmessage);
+	    }
       model.addObject("mlist", uservice.getMessageByUserNum(dto.getU_num()));
       model.addObject("list", list);
       model.addObject("dto", dto);
